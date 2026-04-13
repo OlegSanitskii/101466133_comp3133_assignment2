e4e136537b66c925085a27161ba765ec9e8748b9
OlegSanitskii <olegsanitskii@gmail.com>
2026-04-12 22:29:13 -0400

    Configure frontend GraphQL URL for local and Vercel environments

--- 101466133_comp3133_assignment2/frontend/src/app/app.config.ts
+++ 101466133_comp3133_assignment2/frontend/src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig, inject } from '@angular/core';
+import { ApplicationConfig, inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, HttpHeaders } from '@angular/common/http';
 import { InMemoryCache } from '@apollo/client/core';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,11 +14,17 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideApollo(() => {
       const httpLink = inject(HttpLink);
-      const token = localStorage.getItem('token') || '';
+      const platformId = inject(PLATFORM_ID);
+
+      let token = '';
+
+      if (isPlatformBrowser(platformId)) {
+        token = localStorage.getItem('token') || '';
+      }
 
       return {
         link: httpLink.create({
-          uri: 'http://localhost:4000/graphql',
+          uri: environment.graphqlUrl,
           headers: new HttpHeaders({
             Authorization: token ? `Bearer ${token}` : ''
           })
---
+++ 101466133_comp3133_assignment2/frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  graphqlUrl: 'https://101466133-comp3133-assignment2.vercel.app/graphql'
+};
